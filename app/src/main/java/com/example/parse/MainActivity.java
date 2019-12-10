04a1a6d3162868e4e487ad60f76451297dd552f8
zxm870973 <zxm870973@outlook.com>
2019-12-10 18:54:12 +0800

    抖音无水印下载APP(增加微视、最右无水印解析)

--- app/src/main/java/com/example/parse/MainActivity.java
+++ app/src/main/java/com/example/parse/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.parse;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
 import android.Manifest;
 import android.content.ClipData;
 import android.content.ClipboardManager;
@@ -22,8 +23,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.util.ParsingVideo;
+import com.example.util.VideoUtil;
 
 public class MainActivity extends AppCompatActivity {
     private String payCode = "fkx09508sza6amko2gs8wf6";
@@ -36,12 +36,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (!ParsingVideo.checkStoragePermission(this)) {
+        if (!VideoUtil.getInstance().checkStoragePermission(this)) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     PERMISSIONS_REQUEST_CAMERA);
         }
-        if (!ParsingVideo.checkAlertWindowsPermission(this)) {
+        if (!VideoUtil.getInstance().checkAlertWindowsPermission(this)) {
             startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 0);
         } else {
             showAlertWindows();
@@ -49,18 +49,16 @@ public class MainActivity extends AppCompatActivity {
         etUrl = (EditText) findViewById(R.id.etUrl);
         btnParse = (Button) findViewById(R.id.btnParse);
         tvPath = (TextView) findViewById(R.id.tvPath);
-        tvPath.setText(tvPath.getText() + ParsingVideo.FILE_PATH);
+        tvPath.setText(tvPath.getText() + VideoUtil.FILE_PATH);
         btnParse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String url = etUrl.getText().toString();
                 if (!url.isEmpty()) {
-                    ParsingVideo.parse(MainActivity.this, url);
+                    VideoUtil.getInstance().parse(MainActivity.this, url);
                 }
             }
         });
-
-
     }
 
     class FloatingOnTouchListener implements View.OnTouchListener {
@@ -97,20 +95,21 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case MotionEvent.ACTION_UP:
                     ((ImageView) view).setImageAlpha(255);
+                    VideoUtil.getInstance().writeXY(MainActivity.this, layoutParams.x, layoutParams.y);
                     if (Math.abs(event.getRawX() - lastX) < 5 && Math.abs(event.getRawY() - lastY) < 5) {
                         try {
                             ClipboardManager cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
                             ClipData data = cm.getPrimaryClip();
                             ClipData.Item item = data.getItemAt(0);
                             String url = item.getText().toString();
-                            ParsingVideo.parse(getApplicationContext(), url);
+                            VideoUtil.getInstance().parse(MainActivity.this, url);
                             if (cm != null) {
                                 cm.setPrimaryClip(cm.getPrimaryClip());
                                 cm.setText(null);
                             }
                         } catch (Exception e) {
                             e.printStackTrace();
-                            Toast.makeText(MainActivity.this, "请先复制抖音分享链接", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "请先复制短视频分享链接", Toast.LENGTH_SHORT).show();
                         }
                     }
                 default:
@@ -121,12 +120,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showAlertWindows() {
-        if (ParsingVideo.checkAlertWindowsPermission(this)) {
+        if (VideoUtil.getInstance().checkAlertWindowsPermission(this)) {
             final WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-
             // 新建悬浮窗控件
             ImageView img = new ImageView(getApplicationContext());
-            img.setImageResource(R.mipmap.ic_launcher_round);
+            img.setImageResource(R.mipmap.ic_launcher);
             // 设置LayoutParam
             final WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -139,6 +137,8 @@ public class MainActivity extends AppCompatActivity {
             layoutParams.format = PixelFormat.RGBA_8888;
             layoutParams.width = 96;
             layoutParams.height = 96;
+            layoutParams.x = VideoUtil.getInstance().getX(this);
+            layoutParams.y = VideoUtil.getInstance().getY(this);
             // 将悬浮窗控件添加到WindowManager
             windowManager.addView(img, layoutParams);
             img.setOnTouchListener(new FloatingOnTouchListener(windowManager, layoutParams));
@@ -166,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 0) {
-            if (!ParsingVideo.checkAlertWindowsPermission(this)) {
+            if (!VideoUtil.getInstance().checkAlertWindowsPermission(this)) {
                 Toast.makeText(this, "悬浮窗权限获取失败,如需使用悬浮框功能请手动开启", Toast.LENGTH_LONG).show();
             } else {
                 showAlertWindows();
---
+++ app/src/main/java/com/example/util/VideoUtil.java
@@ -0,0 +1,387 @@
+package com.example.util;
+
+import android.Manifest;
+import android.app.AppOpsManager;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.util.Patterns;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+interface DownLoadListener {
+    void onStartDownload(Context context);
+
+    void onDownloading(Context context, int progress);
+
+    void onErrorDownload(Context context);
+
+    void onFinishDownload(Context context, String path);
+}
+
+
+public class VideoUtil implements DownLoadListener {
+    private static final VideoUtil videoUtil = new VideoUtil();
+    private static final Handler handler = new Handler();
+
+    private VideoUtil() {
+    }
+
+    public static VideoUtil getInstance() {
+        return videoUtil;
+    }
+
+    @Override
+    public void onStartDownload(final Context context) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, "开始下载", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onDownloading(final Context context, final int progress) {
+
+    }
+
+    @Override
+    public void onErrorDownload(final Context context) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, "下载失败", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onFinishDownload(final Context context, final String path) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, "下载成功!文件保存目录:" + path, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    public static final String FILE_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath();
+
+    public void writeXY(Context context, int x, int y) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("x", x);
+        editor.putInt("y", y);
+        editor.commit();
+    }
+
+    public int getX(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("data", Context.MODE_PRIVATE);
+        return sharedPreferences.getInt("x", 0);
+    }
+
+    public int getY(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("data", Context.MODE_PRIVATE);
+        return sharedPreferences.getInt("y", 0);
+    }
+
+    public boolean checkStoragePermission(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkAlertWindowsPermission(Context context) {
+        try {
+            Object object = context.getSystemService(Context.APP_OPS_SERVICE);
+            if (object == null) {
+                return false;
+            }
+            Class localClass = object.getClass();
+            Class[] arrayOfClass = new Class[3];
+            arrayOfClass[0] = Integer.TYPE;
+            arrayOfClass[1] = Integer.TYPE;
+            arrayOfClass[2] = String.class;
+            Method method = localClass.getMethod("checkOp", arrayOfClass);
+            if (method == null) {
+                return false;
+            }
+            Object[] arrayOfObject1 = new Object[3];
+            arrayOfObject1[0] = 24;
+            arrayOfObject1[1] = Binder.getCallingUid();
+            arrayOfObject1[2] = context.getPackageName();
+            int m = ((Integer) method.invoke(object, arrayOfObject1));
+            return m == AppOpsManager.MODE_ALLOWED;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public void parse(final Context context, String url) {
+        if (!checkStoragePermission(context)) {
+            Toast.makeText(context, "没有存储权限", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        final OkHttpClient okHttpClient = new OkHttpClient();
+        Pattern pattern = Patterns.WEB_URL;
+        Matcher matcher = pattern.matcher(url);
+        if (matcher.find()) {
+            url = matcher.group(0);
+            if (url.contains("douyin")) {
+                try {
+                    onStartDownload(context);
+                    Request request = new Request.Builder().url(url).build();
+                    okHttpClient.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            onErrorDownload(context);
+                        }
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+
+                            String html = response.body().string();
+                            Pattern patternCompile = Pattern
+                                    .compile("(?<=playAddr: \")https?://.+(?=\",)");
+                            Matcher matcher = patternCompile.matcher(html);
+                            while (matcher.find()) {
+                                Request request = new Request.Builder().url(matcher.group(0).replaceAll("playwm", "play")).addHeader("User-Agent",
+                                        "Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16D57 Version/12.0 Safari/604.1").build();
+                                Log.e("TAG", matcher.group(0).replaceAll("playwm", "play"));
+                                okHttpClient.newCall(request).enqueue(new Callback() {
+                                    @Override
+                                    public void onFailure(Call call, IOException e) {
+                                        onErrorDownload(context);
+                                    }
+
+                                    @Override
+                                    public void onResponse(Call call, Response response) throws IOException {
+                                        InputStream is = null;
+                                        byte[] buf = new byte[2048];
+                                        int len = 0;
+                                        FileOutputStream fos = null;
+                                        String FILE_NAME = UUID.randomUUID().toString().replaceAll("-", "");
+                                        File file = new File(FILE_PATH + "/抖音无水印/" + FILE_NAME + ".mp4");
+                                        if (!file.getParentFile().exists()) {
+                                            file.getParentFile().mkdirs();
+                                        }
+                                        try {
+                                            is = response.body().byteStream();
+                                            long total = response.body().contentLength();
+                                            fos = new FileOutputStream(file);
+                                            long sum = 0;
+                                            while ((len = is.read(buf)) != -1) {
+                                                fos.write(buf, 0, len);
+                                                sum += len;
+                                                int progress = (int) (sum * 1.0f / total * 100);
+                                                onDownloading(context, progress);
+                                            }
+                                            fos.flush();
+                                            onFinishDownload(context, file.getAbsolutePath());
+                                        } catch (Exception e) {
+                                            onErrorDownload(context);
+                                            e.printStackTrace();
+                                        } finally {
+                                            try {
+                                                if (is != null) {
+                                                    is.close();
+                                                }
+                                                if (fos != null) {
+                                                    fos.close();
+                                                }
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    onErrorDownload(context);
+                }
+            } else if (url.contains("weishi")) {
+                try {
+                    matcher = Pattern.compile("\\w{17}").matcher(url);
+                    if (matcher.find()) {
+                        String feedId = matcher.group(0);
+                        String matchUrl = "https://h5.qzone.qq.com/webapp/json/weishi/WSH5GetPlayPage?feedid=" + feedId;
+                        Request request = new Request.Builder().url(matchUrl).build();
+                        onStartDownload(context);
+                        okHttpClient.newCall(request).enqueue(new Callback() {
+                            @Override
+                            public void onFailure(Call call, IOException e) {
+                                onErrorDownload(context);
+                            }
+
+                            @Override
+                            public void onResponse(Call call, Response response) throws IOException {
+                                String result = response.body().string();
+                                Request request = new Request.Builder().url(JsonUtil.getJsonValue(result, "data.feeds[0].video_url")).build();
+                                okHttpClient.newCall(request).enqueue(new Callback() {
+                                    @Override
+                                    public void onFailure(Call call, IOException e) {
+                                        onErrorDownload(context);
+                                    }
+
+                                    @Override
+                                    public void onResponse(Call call, Response response) {
+                                        InputStream is = null;
+                                        byte[] buf = new byte[2048];
+                                        int len = 0;
+                                        FileOutputStream fos = null;
+                                        String FILE_NAME = UUID.randomUUID().toString().replaceAll("-", "");
+                                        File file = new File(FILE_PATH + "/微视无水印/" + FILE_NAME + ".mp4");
+                                        if (!file.getParentFile().exists()) {
+                                            file.getParentFile().mkdirs();
+                                        }
+                                        try {
+                                            is = response.body().byteStream();
+                                            long total = response.body().contentLength();
+                                            fos = new FileOutputStream(file);
+                                            long sum = 0;
+                                            while ((len = is.read(buf)) != -1) {
+                                                fos.write(buf, 0, len);
+                                                sum += len;
+                                                int progress = (int) (sum * 1.0f / total * 100);
+                                                onDownloading(context, progress);
+                                            }
+                                            fos.flush();
+                                            onFinishDownload(context, file.getAbsolutePath());
+                                        } catch (Exception e) {
+                                            onErrorDownload(context);
+                                            e.printStackTrace();
+                                        } finally {
+                                            try {
+                                                if (is != null) {
+                                                    is.close();
+                                                }
+                                                if (fos != null) {
+                                                    fos.close();
+                                                }
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    onErrorDownload(context);
+                }
+            } else if (url.contains("zuiyou")) {
+                try {
+                    matcher = Pattern.compile("\\d{9}").matcher(url);
+                    if (matcher.find()) {
+                        String pid = matcher.group(0);
+                        url = "https://share.izuiyou.com/api/post/detail";
+                        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), "{\"pid\":" + pid + "}");
+                        Request request = new Request.Builder().url(url).post(body).build();
+                        onStartDownload(context);
+                        okHttpClient.newCall(request).enqueue(new Callback() {
+                            @Override
+                            public void onFailure(Call call, IOException e) {
+                                onErrorDownload(context);
+                            }
+
+                            @Override
+                            public void onResponse(Call call, Response response) throws IOException {
+                                String result = response.body().string();
+                                Request request = new Request.Builder().url(JsonUtil.getJsonValue(result, "data.post.videos." + JsonUtil.getJsonValue(result, "data.post.imgs[0].id") + ".url")).build();
+                                okHttpClient.newCall(request).enqueue(new Callback() {
+                                    @Override
+                                    public void onFailure(Call call, IOException e) {
+                                        onErrorDownload(context);
+                                    }
+
+                                    @Override
+                                    public void onResponse(Call call, Response response) {
+
+                                        InputStream is = null;
+                                        byte[] buf = new byte[2048];
+                                        int len = 0;
+                                        FileOutputStream fos = null;
+                                        String FILE_NAME = UUID.randomUUID().toString().replaceAll("-", "");
+                                        File file = new File(FILE_PATH + "/最右无水印/" + FILE_NAME + ".mp4");
+                                        if (!file.getParentFile().exists()) {
+                                            file.getParentFile().mkdirs();
+                                        }
+                                        try {
+                                            is = response.body().byteStream();
+                                            long total = response.body().contentLength();
+                                            fos = new FileOutputStream(file);
+                                            long sum = 0;
+                                            while ((len = is.read(buf)) != -1) {
+                                                fos.write(buf, 0, len);
+                                                sum += len;
+                                                int progress = (int) (sum * 1.0f / total * 100);
+                                                onDownloading(context, progress);
+                                            }
+                                            fos.flush();
+                                            onFinishDownload(context, file.getAbsolutePath());
+                                        } catch (Exception e) {
+                                            onErrorDownload(context);
+                                            e.printStackTrace();
+                                        } finally {
+                                            try {
+                                                if (is != null) {
+                                                    is.close();
+                                                }
+                                                if (fos != null) {
+                                                    fos.close();
+                                                }
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    onErrorDownload(context);
+                }
+            }
+        } else {
+            Toast.makeText(context, "请复制正确的视频链接", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
